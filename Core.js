d6bc5d1ad1b1762459bdd4b38600ed2bbff0028e
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2022-11-22 21:31:05 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -3640,9 +3640,10 @@ case 'awesomecheck':
                  case 'prettycheck':
                     case 'lovelycheck':
                       case 'uglycheck':
+                        case 'chadcheck':
                         if (isBan) return reply(mess.banned)
                         if (isBanChat) return reply(mess.bangc)
-				if (!text) return replay(`Tag Someone, Example : ${prefix + command} @Apurb`)
+				if (!text) return replay(`Tag Someone, Example : ${prefix + command} @<username>`)
 					const sangeh = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 					const sange = sangeh[Math.floor(Math.random() * sangeh.length)]
 Miku.sendMessage(from, { text: `*${command}*\n\nName : ${q}\nAnswer : *${sange}%*` }, { quoted: m })
@@ -4704,12 +4705,12 @@ quickReplyButton: {
 displayText: '✨Menu✨',
 id: '-menu'
 }  
-}/*, {
+}, {
 quickReplyButton: {
 displayText: 'Bot Owner',//owner no
 id: '-owner'
 }
-}*/
+}
 ]
 let txt = `「 *${global.OwnerName}'s Broadcast* 」\n\n${text}`
 Miku.send5ButImg(yoi, txt, `${global.BotName}`, BotLogo, btn, Thumb)
@@ -4783,7 +4784,7 @@ stickermeme, quotes, darkjoke
 
  *━━━〈  🎐 Fun 🎐  〉━━━*
 
-reaction, truth, dare, couple, soulmate, handsomecheck, beautifulcheck, awesomecheck, greatcheck, gaycheck, cutecheck, lesbiancheck, hornycheck, prettycheck, lovelycheck, uglycheck, charactercheck
+reaction, truth, dare, couple, soulmate, handsomecheck, beautifulcheck, chadcheck,awesomecheck, greatcheck, gaycheck, cutecheck, lesbiancheck, hornycheck, prettycheck, lovelycheck, uglycheck, charactercheck
 
  *━━━〈  🪁 Essentials 🪁  〉━━━*
 
